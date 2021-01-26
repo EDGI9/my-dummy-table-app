4bfe47b6a47b3c867551e060e7b749ba6739ec29
Edgar Dias <edias@evident.nl>
2021-01-26 18:26:09 +0000

    Moving table dummy data to the store

--- my-dummy-table-app/src/store/index.js
+++ my-dummy-table-app/src/store/index.js
@@ -4,8 +4,83 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    tableColumns: [
+      {
+        dataIndex: "name",
+        key: "name",
+        slots: { title: "customTitle" },
+        scopedSlots: { customRender: "name" },
+      },
+      {
+        title: "Age",
+        dataIndex: "age",
+        key: "age",
+      },
+      {
+        title: "Address",
+        dataIndex: "address",
+        key: "address",
+      },
+      {
+        title: "Tags",
+        key: "tags",
+        dataIndex: "tags",
+        scopedSlots: { customRender: "tags" },
+      },
+      {
+        title: "Action",
+        key: "action",
+        scopedSlots: { customRender: "action" },
+      },
+    ],
+    tableData: [
+      {
+        key: "1",
+        name: "John Brown",
+        age: 32,
+        address: "New York No. 1 Lake Park",
+        tags: ["nice", "developer"],
+      },
+      {
+        key: "2",
+        name: "Jim Green",
+        age: 42,
+        address: "London No. 1 Lake Park",
+        tags: ["loser"],
+      },
+      {
+        key: "3",
+        name: "Joe Black",
+        age: 32,
+        address: "Sidney No. 1 Lake Park",
+        tags: ["cool", "teacher"],
+      },
+    ],
+  },
+  getters: {
+    tableColumns: (state) => {
+      return state.tableColumns;
+    },
+    tableData: (state) => {
+      return state.tableData;
+    },
+  },
+  mutations: {
+    addTableRow(state, payload) {
+      this.tableData.push(payload);
+    },
+    removeTableRow(state) {
+      /* Filter out the deleted row */
+    },
+  },
+  actions: {
+    addTableRow(context, payload) {
+      context.commit("addTableRow", payload);
+    },
+    removeTableRow() {
+      /* Filter out the deleted row */
+    },
+  },
+  modules: {},
 });
